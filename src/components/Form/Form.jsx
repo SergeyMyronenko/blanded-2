1fddf8855b503378160f2b02848dd2218ebc04a2
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-02-24 12:01:03 +0200

    task-1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,34 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const [input, setInput] = useState('');
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSabmit = e => {
+    e.preventDefault();
+    onSubmit(input);
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSabmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        value={input}
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        onChange={handleChange}
+      />
+    </form>
+  );
 };
