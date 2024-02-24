e41c6bb88e9ecc9ab5075f149b7c22b3bc530d51
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-02-24 12:48:33 +0200

    finished task-1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,10 @@
+import { FadeLoader } from 'react-spinners';
+import style from './Loader.module.css';
+
 export const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={style.backdrop}>
+      <FadeLoader color="#36d7b7" />
+    </div>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,13 +1,18 @@
 import { getPhotos } from 'apiService/photos';
-import { Form, PhotosGallery, Text } from 'components';
+import { Button, Form, Loader, PhotosGallery, Text } from 'components';
 import { useState, useEffect } from 'react';
 
 export const Photos = () => {
   const [query, setQuery] = useState('');
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   const hendleSearch = query => {
     setQuery(query);
+  };
+  const handleClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
   useEffect(() => {
@@ -15,17 +20,33 @@ export const Photos = () => {
       return;
     }
     const findPhotos = async () => {
-      const { photos } = await getPhotos(query, 1);
-      setPhotos(photos);
+      setIsLoading(true);
+      try {
+        const { photos } = await getPhotos(query, page);
+        setPhotos(prevPhotos => [...prevPhotos, ...photos]);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
     };
     findPhotos();
-  }, [query]);
+  }, [query, page]);
 
   return (
     <>
-      {/* <Text textAlign="center">Let`s begin search 🔎</Text> */}
       <Form onSubmit={hendleSearch} />
+      {photos.length === 0 && (
+        <Text textAlign="center">Let`s begin search 🔎</Text>
+      )}
       <PhotosGallery gallery={photos} />
+
+      {photos.length > 0 && (
+        <Button onClick={handleClick} disabled={isLoading}>
+          Load more
+        </Button>
+      )}
+      {isLoading && <Loader />}
     </>
   );
 };
